b651bacfdc2973e3cbfcf984163c29ecbfd51ffc
Ryan Hawkins <rkhawkins80@hotmail.com>
2023-09-20 15:54:28 -0500

    updated input control for amount of players

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def play_game():
     print()
     num_players = ""
     while num_players not in [1, 2]:
-        num_players = int(
+        num_players = (
             input("\nSelect number of human players (1 or 2):  "))
     player1 = input("Player 1, what is your name?  ")
     current_player = get_initials(player1)
